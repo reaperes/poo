f902962ee8026ac8d39d6817d3292040d404f649
reaperes <emerald105@hanmail.net>
2014-09-25 21:17:03 +0900

    skeleton game engine

---
+++ js/game.js
@@ -0,0 +1,18 @@
+var Game = function () {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  function clear() {
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function update() {
+  }
+
+  function render() {
+  }
+
+  function loop() {
+  }
+};
